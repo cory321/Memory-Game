9cf911fa8d4ce1deb2d2ece7563b68c4ce3c7873
cory321 <cory321@gmail.com>
2015-08-24 13:37:49 -0700

    added new images

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ window.onload = function() {
 var game = function(array) {
 	this.numCards = 16;
 	this.cardArray = array;
-	this.pairingState = 0;
+	this.clickState = 1;
+	this.firstCard = 0;
+	this.secondCard = 0;
 };
 
 game.prototype.shuffle = function() {
@@ -39,7 +41,7 @@ game.prototype.generateGrid = function() {
 	var container = document.getElementById("container");
 
 	for (var i = 1; i <= this.numCards; i++) {
-		html = html + "<div class=\"cell pair-" + this.cardArray[i-1] + "\"><img src=\"images/0"+ this.cardArray[i-1] +".jpg\"></div>";
+		html = html + "<div data="+ this.cardArray[i-1] +" class=\"cell\"><img src=\"images/back.jpg\"></div>";
 	}
 
 	container.innerHTML = html;
@@ -49,14 +51,42 @@ game.prototype.generateGrid = function() {
 game.prototype.addClickListener = function() {
 	var cells = document.querySelectorAll(".cell");
 
+	var click = this.clickState;
+	var firstCard = this.firstCard;
+	var secondCard = this.secondCard;
+
+	var showCard = function(div, i) {
+		var data = div.getAttribute("data");
+		div.innerHTML = "<img src=\"images/0"+ data +".jpg\">";
+	};
+
+	var hideCard = function(div) {
+		div.innerHTML = "<img src=\"images/back.jpg\">";
+	};
+
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].addEventListener("click", function(){
-			console.log("hello");
+			
+			if(click == 2) {
+				click = 1;
+				secondCard = this.getAttribute("data");
+				console.log("Your second card is "+ secondCard);
+				
+				showCard(this, i);
+			} else {
+				click++;
+				firstCard = this.getAttribute("data");
+				console.log("Your first card is "+ firstCard);
+				
+				showCard(this, i);
+			}
+
+			if(firstCard == secondCard) {
+				console.log("MATCH FOUND");
+			} else {
+
+			}
 		});
 	}
-
-game.prototype.checkMatch = function() {
-	
 };
 
-};
